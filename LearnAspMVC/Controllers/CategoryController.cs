ac31313e17345f5f1ab4cb4ea351676f362374cd
Mohammad Baragbah <duniandewon@gmail.com>
2022-05-04 23:20:58 +0700

    Complete CRUD operation

--- LearnAspMVC/Controllers/CategoryController.cs
+++ LearnAspMVC/Controllers/CategoryController.cs
@@ -43,9 +43,57 @@ namespace LearnAspMVC.Controllers
         }
 
         /*Show category details*/
-        public IActionResult Show()
+        public IActionResult Show(int? id)
         {
-            return View();
+            if (id == null || id == 0) return NotFound();
+
+            var category = _db.Categories.Find(id);
+
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+
+            var category = _db.Categories.Find(id);
+
+            if (category == null) return NotFound();
+
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCategory(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+
+            var category = _db.Categories.Find(id);
+
+            if (category == null) return NotFound();
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
