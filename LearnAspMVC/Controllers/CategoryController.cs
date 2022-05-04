29375dafa426fa83ede8139d62b8a9952eae23f4
Mohammad Baragbah <duniandewon@gmail.com>
2022-05-04 15:59:05 +0700

    List categories and create form

---
+++ LearnAspMVC/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using LearnAspMVC.Data;
+using LearnAspMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnAspMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        /*List all categories*/
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Categories;
+
+            return View(categoryList);
+        }
+
+        /* Create a new category (GET) */
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        /* Create a new category (POST) */
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(Category values)
+        {
+            _db.Add(values);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        /*Show category details*/
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
---
+++ LearnAspMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LearnAspMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnAspMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
